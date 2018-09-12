7844a4d21b231876284570fd0eb55447e994e3c7
Ted Barlow <ted@noukod.com>
2018-09-12 15:32:52 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  v=word.scan(/\b[aeiou]/) { |match| }
+  v=word.scan(/\b[aeiou]/) { |match| return true}
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
